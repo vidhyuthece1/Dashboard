9781959e42b57366784e22b722c90c98381f8d76
vidhyuthece1 <92293865+vidhyuthece1@users.noreply.github.com>
2024-09-18 14:41:58 +0530

    CRUD in Products

--- dash_board/inventory/urls.py
+++ dash_board/inventory/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('dashboard',views.index,name="inventory-index"),
     path('staff/',views.staff,name='inventory-staff'),
     path('product/',views.product, name='inventory-product'),
+    path('product/delete/<int:pk>/',views.product_delete,name='inventory-product-delete'),
+    path('product/update/<int:pk>/',views.product_update,name='inventory-product-update'),
     path('order/',views.order,name='inventory-order'),
 ]
--- dash_board/inventory/views.py
+++ dash_board/inventory/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from .forms import ProductForm
 # Create your views here.
 
 @login_required
@@ -13,8 +15,41 @@ def staff(request):
 
 @login_required
 def product(request):
-    return render(request,'inventory/product.html')
- 
+    items = Product.objects.all()
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory-product')
+    else:
+        form = ProductForm()
+    context = {
+        'items' : items,
+        'form' : form,
+    }
+    return render(request,'inventory/product.html',context)
+
+def product_delete(request,pk):
+    item = Product.objects.get(id = pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('inventory-product')
+    return render(request,'inventory/product_delete.html')
+
+def product_update(request,pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory-product')
+    else:
+        form = ProductForm(instance=item)
+    context = {
+        'form': form,
+    }
+    return render(request, 'inventory/product_update.html', context)
+
 @login_required
 def order(request):
     return render(request, 'inventory/order.html')
