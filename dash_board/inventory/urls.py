72189bae45c02d4b606729435e7d2f86526049dd
vidhyuthece1 <92293865+vidhyuthece1@users.noreply.github.com>
2024-09-18 17:33:35 +0530

    staff updated

--- dash_board/inventory/urls.py
+++ dash_board/inventory/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('dashboard',views.index,name="inventory-index"),
     path('staff/',views.staff,name='inventory-staff'),
+    path('staff/detail/<int:pk>',views.staff_detail,name='inventory-staff-detail'),
     path('product/',views.product, name='inventory-product'),
     path('product/delete/<int:pk>/',views.product_delete,name='inventory-product-delete'),
     path('product/update/<int:pk>/',views.product_update,name='inventory-product-update'),
--- dash_board/inventory/views.py
+++ dash_board/inventory/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from .forms import ProductForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 @login_required
@@ -11,7 +12,19 @@ def index(request):
 
 @login_required
 def staff(request):
-    return render (request,'inventory/staff.html')
+    workers = User.objects.all()
+    context = {
+        'workers': workers
+    }
+    return render (request,'inventory/staff.html',context)
+
+@login_required
+def staff_detail(request,pk):
+    workers = User.objects.get(id = pk)
+    context={
+        'workers' : workers,
+    }
+    return render(request,'inventory/staff_detail.html',context)
 
 @login_required
 def product(request):
@@ -29,6 +42,7 @@ def product(request):
     }
     return render(request,'inventory/product.html',context)
 
+@login_required
 def product_delete(request,pk):
     item = Product.objects.get(id = pk)
     if request.method == 'POST':
@@ -36,6 +50,7 @@ def product_delete(request,pk):
         return redirect('inventory-product')
     return render(request,'inventory/product_delete.html')
 
+@login_required
 def product_update(request,pk):
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
